169b6316e43371c7e17abde54fc4687ba095a316
DenizSalk <denizsalk@hotmail.com>
2022-08-04 22:45:42 +0300

    version 0.1 Cleared personal info

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import time
 import pywhatkit
 import keyboard
-message = "Whatsalk cümleten iyi geceler diler. Bu gecelik son mesajım. Mis gibi çalışıyor. Bayramda toplu tebrik hizmetim de olacak. https://www.youtube.com/watch?v=gunZ49SGLg8"
-list = ["+90 532 391 81 54","+90 553 334 34 92","+90 542 543 34 91"]
+message = "Message"
+list = ["Numbers with list form"]
 listleng = str(len(list))
 listlengint = len(list)
-print("listede " + listleng + " farklı numara bulunmaktadır.")
-print("mesajınız: " + message + " || doğru yazdığından emin ol")
+print("Number of list items: " + listleng + " different numbers are found.")
+print("message: " + message + " ||")
 i = 0
 while (i < listlengint):
     a = list[i]
