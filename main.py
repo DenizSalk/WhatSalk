a96bbedd93334ae8d9cd34074626ddb3cd00cf60
salkdeniz <denizsalk2001@gmail.com>
2022-08-05 17:44:01 +0300

    Resim doyası atılan versiyonu

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import pywhatkit
 import keyboard
 import PySimpleGUI as sg
 import pandas as pd
-sg.theme('Blue')
+sg.theme('DarkGreen2')
 layout = [
 
-          [sg.Column([[sg.Image(filename='icon.png')]],justification='center')],
+          [sg.Column([[sg.Image(filename='WHATSALK.png')]],justification='center')],
           [sg.Text('Mesajınızı Girin'), sg.Text(size=(15,1))],
-          [sg.InputText()],
-          [sg.FileBrowse(key="2")],
-          [sg.Button('Tamam')]]
+          [sg.InputText(size=(35, 3))],
+          [sg.Column([[sg.FileBrowse(key="2")],[sg.Button('Tamam')]],justification='center')]]
 
 window = sg.Window("WhatSalk", icon='icon.ico').Layout(layout)
 
@@ -25,20 +24,23 @@ window.close()
 message = str(values[1])
 df_sheet_all = pd.read_excel(values['2'], sheet_name=None)
 print(df_sheet_all)
-mylist = df_sheet_all['Telefon'].tolist()
+mylist = df_sheet_all['Telefon'].values.tolist()
 print(mylist)
 listlengint = len(mylist)
 print("Numara Sayısı: " + str(listlengint) )
 print("Mesaj " + message)
 i = 0
+
+
 while (i < listlengint):
     print(mylist[i])
     a = mylist[i]
-    correcta = "+" + mylist[i]
-    pywhatkit.sendwhatmsg_instantly(correcta, message, 5)
+    correcta = "+90" + str(mylist[i])
+    pywhatkit.sendwhats_image( correcta, "fairy.jpg", "Yazılı test")
     keyboard.press_and_release('enter')
     time.sleep(3)
     keyboard.press_and_release('ctrl+w')
     i = i + 1
+    time.sleep(0.5)
 
 
