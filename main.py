787bdaa8e3b95d7bacfef25e2b776a0682a1edf8
DenizSalk <denizsalk@hotmail.com>
2022-09-13 16:50:24 +0300

    urgent

--- main.py
+++ main.py
@@ -5,14 +5,8 @@ import PySimpleGUI as sg
 import pandas as pd
 sg.theme('DarkGreen2')
 layout = [
-
-          [sg.Column([[sg.Image(filename='WHATSALK.png')]],justification='center')],
-          [sg.Text('Mesajınızı Girin'), sg.Text(size=(15,1))],
-          [sg.InputText(size=(35, 3))],
           [sg.Column([[sg.FileBrowse(key="2")],[sg.Button('Tamam')]],justification='center')]]
-
 window = sg.Window("WhatSalk", icon='icon.ico').Layout(layout)
-
 while True:
     event, values = window.read()
     print(event, values)
@@ -20,27 +14,21 @@ while True:
         break
 event, values = window.read()
 window.close()
-
-message = str(values[1])
 df_sheet_all = pd.read_excel(values['2'], sheet_name=None)
 print(df_sheet_all)
 mylist = df_sheet_all['Telefon'].values.tolist()
 print(mylist)
 listlengint = len(mylist)
 print("Numara Sayısı: " + str(listlengint) )
-print("Mesaj " + message)
 i = 0
-
-
 while (i < listlengint):
     print(mylist[i])
     a = mylist[i]
     correcta = "+90" + str(mylist[i])
-    pywhatkit.sendwhats_image( correcta, "fairy.jpg", "Yazılı test")
+    time.sleep(1)
+    pywhatkit.sendwhats_image( correcta, "a.jpg", " ")
     keyboard.press_and_release('enter')
     time.sleep(3)
     keyboard.press_and_release('ctrl+w')
     i = i + 1
     time.sleep(0.5)
-
-
